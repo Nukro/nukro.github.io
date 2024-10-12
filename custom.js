49ffd7d9392a26aec7338909e96b88dff2782ead
orkunDEV <orkunatasoy1996@live.de>
2024-10-13 01:38:15 +0200

    New Website

--- custom.js
+++
@@ -1,7 +0,0 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
-
-    $('.nav-item.only-mobile').click(function(e){
-       $(this).siblings().not(".only-mobile").toggle();
-    });
-});
